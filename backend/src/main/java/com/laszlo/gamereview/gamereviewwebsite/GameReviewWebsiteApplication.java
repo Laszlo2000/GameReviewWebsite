ea768c4e7a19d26915ae1396eeb422f672f35e6f
Ilonczai László <laszlo2000@mailbox.unideb.hu>
2024-10-26 21:14:17 +0200

    Initial commit

---
+++ backend/src/main/java/com/laszlo/gamereview/gamereviewwebsite/GameReviewWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.laszlo.gamereview.gamereviewwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameReviewWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameReviewWebsiteApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/laszlo/gamereview/gamereviewwebsite/GameReviewWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laszlo.gamereview.gamereviewwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameReviewWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
